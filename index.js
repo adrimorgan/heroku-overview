4df36f84180285303b51a1ae6b3f80a6a04a8e54
Adrián Morente <adrian95morente@gmail.com>
2019-03-11 17:37:37 +0100

    probando despliegue automático en Heroku

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.use(express.logger());
+app.use("/", express.static(__dirname));
+
+var port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log("Servidor escuchando en " + port);
+});
